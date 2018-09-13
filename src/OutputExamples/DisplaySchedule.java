51f8c41018cadf4c49eb52812996c0628272013d
Ammar Ahmed <21ahmeda@elmbrookstudents.org>
2018-09-13 13:51:00 -0500

    Display Schedule

---
+++ src/OutputExamples/DisplaySchedule.java
@@ -0,0 +1,14 @@
+package OutputExamples;
+
+public class DisplaySchedule {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ammar Ahmed");
+		System.out.println("German 3");
+		System.out.println("POE");
+		System.out.println("AP Euro");
+		System.out.println("AP CS A");
+	}
+
+}
