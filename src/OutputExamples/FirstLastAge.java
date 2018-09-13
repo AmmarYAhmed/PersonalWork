f792b72f86fa82a65d971fdd8ad6ae200a83be6d
Ammar Ahmed <21ahmeda@elmbrookstudents.org>
2018-09-13 13:18:09 -0500

    Display first, last, and age

---
+++ src/OutputExamples/FirstLastAge.java
@@ -0,0 +1,12 @@
+package OutputExamples;
+
+public class FirstLastAge {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ammar");
+		System.out.println("Ahmed");
+		System.out.println("age=14");
+	}
+
+}
