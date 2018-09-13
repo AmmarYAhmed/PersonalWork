2d155c4fbfca6a96a302b86fc8225b9ab3583e92
Ammar Ahmed <21ahmeda@elmbrookstudents.org>
2018-09-13 13:12:07 -0500

    Display name seperately

---
+++ src/OutputExamples/FirstNameLastName.java
@@ -0,0 +1,11 @@
+package OutputExamples;
+
+public class FirstNameLastName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Ammar");
+		System.out.print("Ahmed");
+	}
+
+}
