9e3174369ed8f121bab0c834367ff0ae70062c36
Ammar Ahmed <21ahmeda@elmbrookstudents.org>
2018-09-13 14:09:47 -0500

    Display likes and dislikes

---
+++ src/OutputExamples/LikesAndDislikes.java
@@ -0,0 +1,15 @@
+package OutputExamples;
+
+public class LikesAndDislikes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Likes");	System.out.println("Dislikes");
+		System.out.println("");		System.out.println("");
+		System.out.println("");		System.out.println("");
+		System.out.println("");		System.out.println("");
+		System.out.println("");		System.out.println("");
+		System.out.println("");		System.out.println("");
+	}
+
+}
