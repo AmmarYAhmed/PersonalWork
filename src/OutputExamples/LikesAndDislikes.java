c70ea6a17d636cb7a5f98f782af7bf72e55d2626
Ammar Ahmed <21ahmeda@elmbrookstudents.org>
2018-09-17 14:21:38 -0500

    Updated likes ad dislikes program and got it to work

--- src/OutputExamples/LikesAndDislikes.java
+++ src/OutputExamples/LikesAndDislikes.java
@@ -4,12 +4,12 @@ public class LikesAndDislikes {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Likes");	System.out.println("Dislikes");
-		System.out.println("");		System.out.println("");
-		System.out.println("");		System.out.println("");
-		System.out.println("");		System.out.println("");
-		System.out.println("");		System.out.println("");
-		System.out.println("");		System.out.println("");
+		System.out.println("Likes	Dislikes");
+		System.out.println("h	h");		
+		System.out.println("j	j	");		
+		System.out.println("h	hn");		
+		System.out.println("");		
+		System.out.println("");		
 	}
 
 }
