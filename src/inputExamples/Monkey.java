b29d1ff02086ae81ae17ba78e82eaa25c73a9463
Ammar Ahmed <21ahmeda@elmbrookstudents.org>
2018-09-20 13:12:06 -0500

    Modified monkey program

--- src/inputExamples/Monkey.java
+++ src/inputExamples/Monkey.java
@@ -6,7 +6,7 @@ public class Monkey {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	double HeightOfAdultMonkey = 5.5;
-	Scanner input = new Scanner(System.in);
+	Scanner input = new Scanner (System.in);
 	System.out.println("What is the height of an adult monkey?");
 	HeightOfAdultMonkey = input.nextDouble();
 	System.out.println("The height of an adult monkey is " + HeightOfAdultMonkey);
