2be89378ee13dafd9bb69be035fd783780fb96c9
Ammar Ahmed <21ahmeda@elmbrookstudents.org>
2018-10-02 15:07:50 -0500

    Finished working on the test class correctly

--- src/OOP/testClass.java
+++ src/OOP/testClass.java
@@ -2,9 +2,17 @@ package OOP;
 
 public class testClass {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String [] args)
+	{
 
+		Greeter bob = new Greeter();
+		Friend john = new Friend();
+		bob.sayHello();
+		bob.sayHello();
+		bob.sayHello();
+		john.sayMessage();
+		bob.sayHello();
+		
 	}
 
 }
