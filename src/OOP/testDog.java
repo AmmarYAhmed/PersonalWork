8db154858a69ddafaa44d2853bf97e7bc7f459fd
Ammar Ahmed <21ahmeda@elmbrookstudents.org>
2018-10-02 15:08:04 -0500

    correctly made the dog test class

---
+++ src/OOP/testDog.java
@@ -0,0 +1,14 @@
+package OOP;
+
+public class testDog {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Dog annie = new Dog();
+		
+		annie.woof();
+		annie.attack();
+	}
+
+}
