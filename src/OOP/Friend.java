fb7c9b8587c6b549d7daa2324e796a729e4b04fd
Ammar Ahmed <21ahmeda@elmbrookstudents.org>
2018-10-02 15:07:31 -0500

    Made working dog,friend, and greeter Classes

---
+++ src/OOP/Friend.java
@@ -0,0 +1,11 @@
+package OOP;
+
+public class Friend {
+
+	public void sayMessage()
+	{
+		
+		System.out.println("You are a great friend");
+		
+	}
+}
--- src/OOP/Greeter.java
+++ src/OOP/Greeter.java
@@ -1,10 +1,18 @@
 package OOP;
 
-public class Greeter {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class Greeter{
+//instance fields - data storage (There is no instance field in this program)
+	
+//constructors - methods for constructing the object	
+	
+//default constructor
+	public Greeter()
+	{
+		//Statements to initialize the state of the object
 	}
-
+	//methods - things the object can do
+	public void sayHello()
+	{
+		System.out.println("Hello");
+	}
 }
