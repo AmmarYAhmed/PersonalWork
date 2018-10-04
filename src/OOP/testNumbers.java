cbaae46c1563e52c79ebde54f3402ccf002ed3b2
Ammar Ahmed <21ahmeda@elmbrookstudents.org>
2018-10-04 14:08:14 -0500

    Made program to test the Numbers class

---
+++ src/OOP/testNumbers.java
@@ -0,0 +1,13 @@
+package OOP;
+
+public class testNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Numbers bob = new Numbers();
+		bob.sayNumber(7.5);
+		bob.sayNumberPlus2(4);
+	}
+
+}
