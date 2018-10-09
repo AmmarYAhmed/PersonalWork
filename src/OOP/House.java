7617dcf7a3986ece9217e5ce5abcabe46e028c2e
Ammar Ahmed <21ahmeda@elmbrookstudents.org>
2018-10-08 22:33:34 -0500

    Improved the House class

--- src/OOP/House.java
+++ src/OOP/House.java
@@ -1,5 +1,34 @@
 package OOP;
 
+import java.util.Scanner;
 public class House {
 
+	public House()
+	{
+		
+	}
+	
+	public void fillTheBox()
+	{
+		Scanner input = new Scanner(System.in); 
+		System.out.println("What is the length of the sandbox?");
+		int length = input.nextInt();
+		System.out.println("What is the width of the sandbox?");
+		int width = input.nextInt();
+		System.out.println("What is the depth of the sandbox?");
+		int depth = input.nextInt();
+		System.out.println("How much sand can you transport per hour?");
+		int transport = input.nextInt();
+		int volume = (length*width*depth);
+		System.out.println("It will take " + volume/transport + " hours to fill the sandbox.");
+	}
+	
+	public double fillTheBox2(int length,int width,int depth, int transport)
+	{
+		Scanner input = new Scanner(System.in); 
+		
+		int volume = (length*width*depth);
+		int hours = volume/transport;
+		3return hours;
+	}
 }
