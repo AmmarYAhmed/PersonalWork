3ac2d29bba8310d5c8cf34927c3bb027a100f79c
Ammar Ahmed <21ahmeda@elmbrookstudents.org>
2018-10-11 14:12:12 -0500

    changed iso trap class

--- src/OOP/IsoscelesTrapezoid.java
+++ src/OOP/IsoscelesTrapezoid.java
@@ -2,4 +2,34 @@ package OOP;
 
 public class IsoscelesTrapezoid {
 
+	private int base1;
+	private int base2;
+	private int height;
+	private int leg;
+	
+	public IsoscelesTrapezoid()
+	{
+		base1 = 12;
+		base2 = 32;
+		height = 12;
+		leg= 2;
+	}
+	
+	public IsoscelesTrapezoid(int base1, int base2, int height, int leg)
+	{
+		
+	}
+	
+	public double perimeter()
+	{
+		double perimeter = base1+base2+leg;
+		return perimeter;
+	}
+	
+	public double area()
+	{
+		double area = base1*height;
+		return area;
+	}
+	
 }
