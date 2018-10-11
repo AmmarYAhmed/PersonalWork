a48f874c38d50a85f2e82def75cd6b73173ac812
Ammar Ahmed <21ahmeda@elmbrookstudents.org>
2018-10-11 14:11:16 -0500

    finished right triangle class

--- src/OOP/RightTriangle.java
+++ src/OOP/RightTriangle.java
@@ -11,6 +11,27 @@ public class RightTriangle {
 		height = 23;
 	}
 	
+	public RightTriangle(int base, int height)
+	{
 	
+	}
+	
+	public double hypot()
+	{
+		double hypot = (base*base)+(height*height);
+		return hypot;
+	}
+	
+	public double perimeter()
+	{
+		double perimeter = (base+base)+(height+height);
+		return perimeter;
+	}
+	
+	public double area()
+	{
+		double area = base*height;
+		return area;
+	}
 	
 }
