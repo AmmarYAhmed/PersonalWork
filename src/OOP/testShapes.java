7ac8fa40e7d4beae82a3d71196920731ebe8a3ec
Ammar Ahmed <21ahmeda@elmbrookstudents.org>
2018-10-11 21:36:53 -0500

    started working on the test class assignment

--- src/OOP/testShapes.java
+++ src/OOP/testShapes.java
@@ -5,6 +5,14 @@ public class testShapes {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Rectangle bob = new Rectangle();
+		bob.getLength();
+		bob.getWidth();
+		Circle fred = new Circle();
+		fred.getRadius();
+		RightTriangle jimbo = new RightTriangle();
+		jimbo.get
+		
 	}
 
 }
