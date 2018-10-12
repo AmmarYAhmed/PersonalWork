d87204617088788433a00dc9d5d76bffcbc1e682
Ammar Ahmed <21ahmeda@elmbrookstudents.org>
2018-10-11 21:36:35 -0500

    Improved the iso trap class

--- src/OOP/IsoscelesTrapezoid.java
+++ src/OOP/IsoscelesTrapezoid.java
@@ -20,15 +20,35 @@ public class IsoscelesTrapezoid {
 		
 	}
 	
+	public double getBase1()
+	{
+		return base1;
+	}
+	
+	public double getBase2()
+	{
+		return base2;
+	}
+	
+	public double getHeight()
+	{
+		return height;
+	}
+	
+	public double getLeg()
+	{
+		return leg;
+	}
+	
 	public double perimeter()
 	{
-		double perimeter = base1+base2+leg;
+		double perimeter = base1+base2+leg+leg;
 		return perimeter;
 	}
 	
 	public double area()
 	{
-		double area = base1*height;
+		double area = (base1*base2)*(height/2);
 		return area;
 	}
 	
