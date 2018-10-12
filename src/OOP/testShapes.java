67b15f8bc130caab7c13ea328f31ad2eb8f1edce
Ammar Ahmed <21ahmeda@elmbrookstudents.org>
2018-10-12 14:12:10 -0500

    successfully completed  the testing

--- src/OOP/testShapes.java
+++ src/OOP/testShapes.java
@@ -6,13 +6,35 @@ public class testShapes {
 		// TODO Auto-generated method stub
 
 		Rectangle bob = new Rectangle();
-		bob.getLength();
-		bob.getWidth();
+		bob.setLength(5);
+		System.out.println(bob.getLength());
+		bob.setWidth(7);
+		System.out.println(bob.getWidth());
 		Circle fred = new Circle();
-		fred.getRadius();
-		RightTriangle jimbo = new RightTriangle();
-		jimbo.get
-		
+		fred.setRadius(67);
+		System.out.println(fred.getRadius());
+		RightTriangle jim = new RightTriangle();
+		jim.setBase(2);
+		System.out.println(jim.getBase());
+		jim.setHeight(2);
+		System.out.println(jim.getHeight());
+		IsoscelesTrapezoid john = new IsoscelesTrapezoid();
+		john.setBase1(3);
+		System.out.println(john.getBase1());
+		john.setBase2(3);
+		System.out.println(john.getBase2());
+		john.setHeight(3);
+		System.out.println(john.getHeight());
+		john.setLeg(3);
+		System.out.println(john.getLeg());
+		System.out.println(bob.area());
+		System.out.println(bob.perimeter());
+		System.out.println(fred.area());
+		System.out.println(fred.circumference());
+		System.out.println(jim.area());
+		System.out.println(jim.perimeter());
+		System.out.println(john.area());
+		System.out.println(john.perimeter());
 	}
 
 }
