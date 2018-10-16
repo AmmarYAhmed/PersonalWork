af0a21a05661c6f9715c10582508a7577180a013
Ammar <21ahmeda@elmbrookstudents.org>
2018-10-16 15:08:00 -0500

    started shoe activity

---
+++ src/decisions/shoeActivity.java
@@ -0,0 +1,17 @@
+package decisions;
+
+import java.util.Scanner;
+public class shoeActivity {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in);
+		System.out.println("What is your height and shoe size?");
+		double height = input.nextDouble();
+		double shoeSize = input.nextDouble();
+		System.out.println("height" == height);
+		
+	}
+
+}
