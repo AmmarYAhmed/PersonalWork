674dc052c1e7eecb6d852e0e52c7955ce071a9bb
Ammar <21ahmeda@elmbrookstudents.org>
2018-10-17 15:09:19 -0500

    started grade percentage program

---
+++ src/decisions/gradePercentage.java
@@ -0,0 +1,18 @@
+package decisions;
+
+import java.util.Scanner;
+public class gradePercentage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in);
+		System.out.println("Give me three test scores");
+		double test1 = input.nextDouble();
+		double test2 = input.nextDouble();
+		double test3 = input.nextDouble();
+		System.out.println("Did you do the extra credit?");
+		
+	}
+
+}
