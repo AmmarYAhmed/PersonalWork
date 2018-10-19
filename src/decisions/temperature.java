1b54711bb86167f342c824aff6ca9f1a117b15b8
Ammar <21ahmeda@elmbrookstudents.org>
2018-10-19 15:05:18 -0500

    started temperature activity

---
+++ src/decisions/temperature.java
@@ -0,0 +1,22 @@
+package decisions;
+
+import java.util.Scanner;
+public class temperature {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in);
+		System.out.println("What is the temperature outside?");
+		int temperature=input.nextInt();
+		if (temperature>83)
+		{
+			System.out.println("You can go swim");
+		}
+		else
+		{
+			if (temperature)
+		}
+	}
+
+}
