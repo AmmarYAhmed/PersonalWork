aa6433a7e8be7200e322ad2e18116e23282629d9
Ammar <21ahmeda@elmbrookstudents.org>
2018-10-25 14:08:11 -0500

    updated second counter program

--- src/decisions/counter2.java
+++ src/decisions/counter2.java
@@ -1,10 +1,19 @@
 package decisions;
 
+import java.util.Scanner;
 public class counter2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner input = new Scanner(System.in);
+		System.out.println("What is your favorite number?");
+		int num=input.nextInt();
+		int counter = 0;
+		for(counter=num;)
+		{
+			System.out.println(counter + " Hello");
+		}
 	}
 
 }
