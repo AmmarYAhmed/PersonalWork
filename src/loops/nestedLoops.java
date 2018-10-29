4ee28c94294e7f5c091ab935474c44337379e2e0
Ammar <21ahmeda@elmbrookstudents.org>
2018-10-29 15:05:58 -0500

    started working with nested loops

---
+++ src/loops/nestedLoops.java
@@ -0,0 +1,19 @@
+package loops;
+
+public class nestedLoops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int number = 0;
+		for(number = 1; number <=5; number++ )
+		{
+			System.out.print(number + "   ");
+			if (number == 3)
+			{
+				System.out.print(" 1 ");
+			}
+		}
+	}
+
+}
