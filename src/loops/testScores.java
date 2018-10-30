5088d9b231ed063225c84bbc4134566c8ffb0af2
Ammar <21ahmeda@elmbrookstudents.org>
2018-10-30 15:08:23 -0500

    improved testscores

--- src/loops/testScores.java
+++ src/loops/testScores.java
@@ -1,10 +1,14 @@
 package loops;
 
+import java.util.Scanner;
 public class testScores {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner input = new Scanner(System.in);
+		System.out.println("Gimme a test score");
+		double test = input.nextDouble();
 	}
 
 }
