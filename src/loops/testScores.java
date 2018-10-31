b89182a592066f642bd3d9b3abcae224167c6721
Ammar Ahmed <21ahmeda@elmbrookstudents.org>
2018-10-30 21:08:03 -0500

    tried to get testscores working

--- src/loops/testScores.java
+++ src/loops/testScores.java
@@ -9,6 +9,21 @@ public class testScores {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Gimme a test score");
 		double test = input.nextDouble();
+		double sum = test;
+		double count= 1;
+		
+		while (test != 9999)
+		{
+			System.out.println("Gimme a test score");
+			test = input.nextDouble();
+			if (test>= 70)
+			{
+				sum = sum+test;
+				count++;
+			}
+		}
+		System.out.println(sum);
+		System.out.println(sum/count);
 	}
 
 }
