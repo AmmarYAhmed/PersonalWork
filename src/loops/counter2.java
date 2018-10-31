0d4e06a003b05a78d80247086d509cda1c1cff63
Ammar Ahmed <21ahmeda@elmbrookstudents.org>
2018-10-30 20:52:24 -0500

    organized programs

--- src/loops/counter2.java
+++ src/loops/counter2.java
@@ -1,6 +1,7 @@
 package loops;
 
 import java.util.Scanner;
+
 public class counter2 {
 
 	public static void main(String[] args) {
@@ -9,8 +10,7 @@ public class counter2 {
 		Scanner input = new Scanner(System.in);
 		int total = 0;
 		int counter = 0;
-		for(counter=1; counter<=10; counter++)
-		{
+		for (counter = 1; counter <= 10; counter++) {
 			total += counter;
 		}
 		System.out.println(total);
--- src/loops/cube.java
+++ src/loops/cube.java
@@ -1,6 +1,7 @@
 package loops;
 
 import java.util.Scanner;
+
 public class cube {
 
 	public static void main(String[] args) {
@@ -9,15 +10,14 @@ public class cube {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Gimme a number");
 		int number = input.nextInt();
-		
-		while (number != -1)
-		{
-			System.out.println(number*number*number);
+
+		while (number != -1) {
+			System.out.println(number * number * number);
 			System.out.println("Gimme a number");
-		    number = input.nextInt();
-			
+			number = input.nextInt();
+
 		}
-		
+
 	}
 
 }
