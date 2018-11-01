0927dae3326afe50156e5857a197d472b0e50d8e
Ammar Ahmed <21ahmeda@elmbrookstudents.org>
2018-10-31 21:39:55 -0500

    started classAverage

---
+++ src/loops/classAverage.java
@@ -0,0 +1,13 @@
+package loops;
+
+public class classAverage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int students = 0;
+		
+		System.out.println("How many students are in the class?");
+	}
+
+}
