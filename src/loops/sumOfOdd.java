042adac7d86721a9f5735abe94141bf10108366f
Ammar Ahmed <21ahmeda@elmbrookstudents.org>
2018-10-31 21:39:45 -0500

    tried to get sum of odds working

--- src/loops/sumOfOdd.java
+++ src/loops/sumOfOdd.java
@@ -7,12 +7,14 @@ public class sumOfOdd {
 		// TODO Auto-generated method stub
 
 		Scanner input = new Scanner(System.in);
-		int number = 0;
+		int number = 1;
 		int limit = 0;
 		
 		for (number = 1; number < limit; number=number+2 )
 		{
 			System.out.println("Gimme a number");
+		    limit = input.nextInt();
+			System.out.println(number);
 			
 		}
 	}
