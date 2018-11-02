944c3f214b34281061d1e440394ce231539db60b
Ammar Ahmed <21ahmeda@elmbrookstudents.org>
2018-11-01 22:48:11 -0500

    started prime factorization program

---
+++ src/loops/primeFactorization.java
@@ -0,0 +1,20 @@
+package loops;
+
+public class primeFactorization {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num=0;
+		int factor = 0;
+		
+		for (factor = num; factor<=2; factor=factor-1 )
+		{
+			if (num%factor==0)
+			{
+				
+			}
+		}
+	}
+
+}
