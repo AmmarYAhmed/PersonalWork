a6ccb090c9505aec088afadf4bc91ac42a15bf91
Ammar Ahmed <21ahmeda@elmbrookstudents.org>
2018-11-05 15:07:01 -0600

    started rollAdie program oppurtunity

---
+++ src/randomNumbers/rollAdie.java
@@ -0,0 +1,27 @@
+package randomNumbers;
+
+import java.util.Scanner;
+import java.util.Random;
+public class rollAdie {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner (System.in);
+		Random die = new Random ();
+		int min = 1;
+		System.out.println("What is the amount of sides on the die" );
+		int max = input.nextInt();
+		System.out.println("What is the amount of rolls" );
+		int rolls = input.nextInt();
+		
+		if (max>=min)
+		{
+		for (int roll=1; roll)
+		{
+			
+		}
+		}s
+	}
+
+}
