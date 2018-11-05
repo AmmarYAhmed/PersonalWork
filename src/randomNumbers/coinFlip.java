24d901815e34da11f341298ff2eaf04d0cc9b534
Ammar Ahmed <21ahmeda@elmbrookstudents.org>
2018-11-05 14:24:16 -0600

    made a coin flip program

---
+++ src/randomNumbers/coinFlip.java
@@ -0,0 +1,34 @@
+package randomNumbers;
+
+import java.util.Random;
+public class coinFlip {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Random coin = new Random ();
+		int min = 1;
+		int max = 2;
+		int flip = 0;
+		int side = min + coin.nextInt (max - min + 1);
+		int heads = 0;
+		int tails = 0;
+		
+		for (flip=1; flip<=100000; flip++)
+		{
+			side = min + coin.nextInt (max - min + 1);
+			if (side==1)
+			{
+				heads++;
+			}
+			else
+			{
+				tails++;
+			}
+		}
+		
+		System.out.println(heads + " heads flipped");
+		System.out.println(tails + " tails flipped");
+	}
+
+}
