e1db5f4a4568adbee2aaa57d5ae9b7f758a2d42a
Ammar Ahmed <21ahmeda@elmbrookstudents.org>
2018-11-05 14:43:33 -0600

    made russian first grader program

---
+++ src/randomNumbers/russianFirstGraders.java
@@ -0,0 +1,25 @@
+package randomNumbers;
+import java.util.Random;
+public class russianFirstGraders {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Random height = new Random (5);
+		int min = 26;
+		int max = 43;
+		int each = 0;
+		int above = 0;
+		
+		for (int kid = 1; kid<=248; kid++ )
+		{
+			each = min + height.nextInt (max - min + 1);
+			if (each>36)
+			{
+				above++;
+			}
+		}
+		System.out.println(above + " Russian first graders above 3 feet");
+	}
+
+}
