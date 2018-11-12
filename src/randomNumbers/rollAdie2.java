0b1ff6117f885b998ace5aaa52850dc4f8848843
Ammar Ahmed <21ahmeda@elmbrookstudents.org>
2018-11-12 14:18:43 -0600

    finished roll a die v2

--- src/randomNumbers/rollAdie2.java
+++ src/randomNumbers/rollAdie2.java
@@ -15,15 +15,46 @@ public class rollAdie2 {
 		int rolls = input.nextInt();
 		int side = 0;
 		int ones = 0;
+		int twos = 0;
+		int threes = 0;
+		int fours = 0;
+		int fives = 0;
+		int sixs = 0;
 		
 		for (int roll = 1; roll<=rolls; roll++)
 		{
 			side = min+die.nextInt(max-min+1);
 			if (side==1)
 			{
-				
+				ones ++;
+			}
+			if (side==2)
+			{
+				twos ++;
+			}
+			if (side==3)
+			{
+				threes ++;
+			}
+			if (side==4)
+			{
+				fours ++;
+			}
+			if (side==5)
+			{
+				fives ++;
+			}
+			if (side==6)
+			{
+				sixs ++;
 			}
 		}
+		System.out.println(ones + " ones");
+		System.out.println(twos + " twos");
+		System.out.println(threes + " threes");
+		System.out.println(fours + " fours");
+		System.out.println(fives + " fives");
+		System.out.println(sixs + " sixs");
 	}
 
 }
