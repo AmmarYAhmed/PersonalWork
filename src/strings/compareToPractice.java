c6d5bcfde87575cc8f0d34b31f90f85cc0b51dfb
Ammar Ahmed <21ahmeda@elmbrookstudents.org>
2018-11-12 15:05:12 -0600

    compareToPractice

---
+++ src/strings/compareToPractice.java
@@ -0,0 +1,15 @@
+package strings;
+
+import java.util.Scanner;
+public class compareToPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in);
+		String word1 = input.nextLine();
+		String word2 = input.nextLine();
+		System.out.println(word1.compareTo(word2));
+	}
+
+}
