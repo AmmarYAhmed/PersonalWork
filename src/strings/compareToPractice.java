3ff1edbcf43ea09f9a7f70c4ec63e39bbd363c22
Ammar Ahmed <21ahmeda@elmbrookstudents.org>
2018-11-13 14:06:32 -0600

    finished compareTo

--- src/strings/compareToPractice.java
+++ src/strings/compareToPractice.java
@@ -7,13 +7,23 @@ public class compareToPractice {
 		// TODO Auto-generated method stub
 
 		Scanner input = new Scanner(System.in);
+		System.out.println("first word?");
 		String word1 = input.nextLine();
+		System.out.println("second word?");
 		String word2 = input.nextLine();
+		if ((word1.compareTo(word2)>0))
+		{
+			System.out.println("second word comes before first");
+		}
+		if ((word1.compareTo(word2)==0))
+		{
+			System.out.println("words are the same");
+		}
 		if ((word1.compareTo(word2)<0))
 		{
-			
+			System.out.println("first word comes before second");
 		}
-		System.out.println((word1.compareTo(word2)));
+		
 	}
 
 }
