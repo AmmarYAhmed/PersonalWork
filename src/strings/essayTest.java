65a45645585b344a4cc78fe05ad554b216d8924b
Ammar Ahmed <21ahmeda@elmbrookstudents.org>
2018-11-15 14:02:41 -0600

    made test class for essay

---
+++ src/strings/essayTest.java
@@ -0,0 +1,14 @@
+package strings;
+
+public class essayTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Essay bob = new Essay();
+		
+		System.out.println(bob.capitalLastName());
+		System.out.println(bob.firstName());
+	}
+
+}
