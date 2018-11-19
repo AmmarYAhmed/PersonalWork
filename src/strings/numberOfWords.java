eaca5299b1ac384248d626a53c54fc49bab9679b
Ammar Ahmed <21ahmeda@elmbrookstudents.org>
2018-11-18 22:17:10 -0600

    started num of words

---
+++ src/strings/numberOfWords.java
@@ -0,0 +1,22 @@
+package strings;
+
+import java.util.Scanner;
+public class numberOfWords {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in);
+		String sentence = input.nextLine();
+		int words = 1;
+		for(int i=0; i<sentence.length(); i++)
+		{
+			if(sentence.charAt(i)==' ')
+			{
+				words++;
+				word = sentence.substring(beginIndex, endIndex)
+			}
+		}
+	}
+
+}
