6aa8c8320f29693a882be5bdc1c7c3c960fb1478
Ammar Ahmed <21ahmeda@elmbrookstudents.org>
2018-11-26 14:58:03 -0600

    did randomTestScore activity

---
+++ src/arrays/randomTestScores.java
@@ -0,0 +1,33 @@
+package arrays;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class randomTestScores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Random test = new Random();
+		Scanner input= new Scanner(System.in);
+		int[] tests = new int[250];
+		int largest = 0;
+		int min =  0;
+		int max = 1000;
+		
+		for (int i = 0; i<250; i++)
+		{
+		System.out.print("Test Score: ");
+			tests[i] = min + test.nextInt (max - min + 1);
+			System.out.print(tests[i]);
+			if (tests[i]>largest)
+			{
+				largest = tests[i];
+			}
+			System.out.println();
+		}
+		System.out.println(largest);
+		input.close();
+	}
+
+}
