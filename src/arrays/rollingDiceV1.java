32096216728db019dabc9248aa01c99036a1b987
Ammar Ahmed <21ahmeda@elmbrookstudents.org>
2018-12-03 15:07:26 -0600

    changed rollingDice v1

--- src/arrays/rollingDiceV1.java
+++ src/arrays/rollingDiceV1.java
@@ -1,10 +1,22 @@
 package arrays;
 
+import java.util.Scanner;
 public class rollingDiceV1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner input = new Scanner (System.in);
+		System.out.println("Number of sides");
+		int sides = input.nextInt();
+		System.out.println("Number of students");
+		int students = input.nextInt();
+		int [][] rolls = new int [students][sides];
+		for (int row=0; row<rolls.length;row++)
+		{
+			
+		}
+				
 	}
 
 }
