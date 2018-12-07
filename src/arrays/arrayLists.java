37fed661efc2e8341bf8caa5a22b72587b379020
Ammar Ahmed <21ahmeda@elmbrookstudents.org>
2018-12-07 15:05:31 -0600

    changed arrayLists

--- src/arrays/arrayLists.java
+++ src/arrays/arrayLists.java
@@ -1,22 +1,31 @@
 package arrays;
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.Random;
 public class arrayLists {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Scanner input = new Scanner(System.in);
-		String name = "";
-		ArrayList<Friend> names = new ArrayList<Friend>();
-		names.add(new Friend("Bob",23));
-		names.add(new Friend("Billy",23));
-		names.add(0, new Friend("Joe",23));
-		
-		for (int j = 0; j<names.size(); j++)
+		Random generator = new Random();
+		int max = 70;
+		int min = 50;
+		ArrayList<Rectangle> shapes = new ArrayList<Rectangle>();
+		for(int j=0; j<=400; j++)
 		{
-			System.out.println(names.get(j).getAge());
+			int length =  min + generator.nextInt (max - min + 1);
+			int width =  min + generator.nextInt (max - min + 1);
+			shapes.add(new Rectangle(length,width));
 		} 
+		Rectangle gone = shapes.remove(shapes.size()-1);
+		shapes.set(0, gone);
+		System.out.println(gone.getLength());
+		for(int j=399; j>=0; j--)
+		{
+			System.out.print("Rectangle " + (j+1) + ": " );
+			System.out.print(shapes.get(j).getLength());
+			System.out.print(", " + shapes.get(j).getWidth());
+			System.out.println();
+		}
 	}
 
 }
