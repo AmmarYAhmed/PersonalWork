7db988e8bf2e41eea4103992a7dec433cb7c3e48
Ammar Ahmed <21ahmeda@elmbrookstudents.org>
2018-12-09 22:00:50 -0600

    started rectangle program opp.

---
+++ src/arrays/rectangleArrayList.java
@@ -0,0 +1,25 @@
+package arrays;
+
+import java.util.Random;
+import java.util.ArrayList;
+public class rectangleArrayList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Random generator = new Random();
+		ArrayList<Rectangle> shapes = new ArrayList<Rectangle>();
+		int max = 50;
+		int min = 1;
+		int length =0;
+		int width = 0;
+		for (int k=0; k<20; k++)
+		{
+			length = min + generator.nextInt (max - min + 1);
+			width = min + generator.nextInt (max - min + 1);
+			shapes.add(new Rectangle(length,width));
+		}
+		
+	}
+
+}
