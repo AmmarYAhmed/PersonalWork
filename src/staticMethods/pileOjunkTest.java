7ce99b24216090be9b109282cf05223503ee3284
Ammar Ahmed <21ahmeda@elmbrookstudents.org>
2018-12-13 14:08:05 -0600

    pile o junk test

---
+++ src/staticMethods/pileOjunkTest.java
@@ -0,0 +1,13 @@
+package staticMethods;
+
+public class pileOjunkTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		pileOjunk bob = new pileOjunk();
+		System.out.println(bob.numbers(1, 2));
+		bob.override(3);
+	}
+
+}
