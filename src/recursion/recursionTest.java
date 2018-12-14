97dcb5202ed6a429ab19882774352491df719a5e
Ammar Ahmed <21ahmeda@elmbrookstudents.org>
2018-12-14 15:09:15 -0600

    work

---
+++ src/recursion/recursionTest.java
@@ -0,0 +1,10 @@
+package recursion;
+
+public class recursionTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/recursion/sumTheNumbers.java
@@ -0,0 +1,10 @@
+package recursion;
+
+public class sumTheNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
