e465d6f13088b273f152da70962b91d9cfa67006
Ammar Ahmed <21ahmeda@elmbrookstudents.org>
2018-12-17 20:18:07 -0600

    updated recursion test

--- src/recursion/recursionTest.java
+++ src/recursion/recursionTest.java
@@ -5,7 +5,13 @@ public class recursionTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	
+		sumTheNumbers bob = new sumTheNumbers();
+		sumOfOdd frank = new sumOfOdd();
+
+		System.out.println(bob.sumNum(3));
+		System.out.println(frank.sumOdd(1));
+
 	}
 
 }
+
