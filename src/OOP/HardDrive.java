4ccb9f268064ca908bcf7270bc88fc1224eb180e
Ammar Ahmed <21ahmeda@elmbrookstudents.org>
2018-12-19 15:08:29 -0600

    update

--- src/OOP/HardDrive.java
+++ src/OOP/HardDrive.java
@@ -1,5 +1,20 @@
 package OOP;
 
 public class HardDrive {
+private String name;
+private int file;
+private int capacity;
+private int space;
 
+public HardDrive(String xName, int xFile, int xCapacity, int xSpace)
+{
+	name=xName;
+	file=xFile;
+	capacity= xCapacity;
+	space= xSpace;
+} 
+public void addFile()
+{
+	
+}
 }
