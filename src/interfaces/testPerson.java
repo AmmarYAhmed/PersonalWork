1c0b4c7d071fcc394b6161ecaeb8a348ab4335bd
Ammar Ahmed <21ahmeda@elmbrookstudents.org>
2019-01-04 15:08:54 -0600

    test person

--- src/interfaces/testPerson.java
+++ src/interfaces/testPerson.java
@@ -4,7 +4,18 @@ public class testPerson {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Person bob = new Student();
+		System.out.println(bob.getAge());
+		bob.setName("Bob");
+		System.out.println(bob.getName());
+		System.out.println(bob.toString());
+		
+		Person frank = new Student();
+		System.out.println(frank.getAge());
+		frank.setName("Frank");
+		System.out.println(frank.getName());
+		System.out.println(frank.toString());
+		
 	}
 
 }
