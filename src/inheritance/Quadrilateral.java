c0fca414d21eb0ffd6eb4f1f116cdf2be98e4892
Ammar Ahmed <21ahmeda@elmbrookstudents.org>
2019-01-10 14:08:50 -0600

    quad class

---
+++ src/inheritance/Quadrilateral.java
@@ -0,0 +1,70 @@
+package inheritance;
+
+public class Quadrilateral {
+private double side1;
+private double side2;
+private double side3;
+private double side4;
+
+public Quadrilateral ()
+{
+side1 = 2.0;	
+side2 = 2.0;	
+side3 = 2.0;	
+side4 = 2.0;	
+}
+
+public Quadrilateral (int xSide1, int xSide2, int xSide3, int xSide4)
+{
+	side1 = xSide1;
+	side2 = xSide2;
+	side3 = xSide3;
+	side4 = xSide4;
+}
+
+public void setSide1 (double newSide1)
+{
+	side1 = newSide1;
+}
+
+public void setSide2 (double newSide2)
+{
+	side2 = newSide2;
+}
+
+public void setSide3 (double newSide3)
+{
+	side3 = newSide3;
+}
+
+public void setSide4 (double newSide4)
+{
+	side4 = newSide4;
+}
+
+public double getSide1 ()
+{
+	return side1;
+}
+
+public double getSide2 ()
+{
+	return side2;
+}
+
+public double getSide3 ()
+{
+	return side3;
+}
+
+public double getSide4 ()
+{
+	return side4;
+}
+
+public double perimeter ()
+{
+	return side1+side2+side3+side4;
+}
+
+}
