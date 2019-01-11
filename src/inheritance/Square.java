bab0bbb0464745e06677fadba4f5a721c46f0e08
Ammar Ahmed <21ahmeda@elmbrookstudents.org>
2019-01-11 15:07:08 -0600

    square

---
+++ src/inheritance/Square.java
@@ -0,0 +1,14 @@
+package inheritance;
+
+public class Square extends Rectangle implements Geo {
+
+	public Square()
+	{
+		super();
+	}
+	
+	public Square(double side)
+	{
+		super(side,side);
+	}
+}
