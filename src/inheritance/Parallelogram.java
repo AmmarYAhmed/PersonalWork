0d30d5019bc91833344a9cf3133234a7819f8dba
Ammar Ahmed <21ahmeda@elmbrookstudents.org>
2019-01-11 15:07:03 -0600

    parallelogram

---
+++ src/inheritance/Parallelogram.java
@@ -0,0 +1,34 @@
+package inheritance;
+
+public class Parallelogram extends Quadrilateral implements Geo{
+
+	private double height;
+		public Parallelogram()
+		{
+			super();
+			height=4;
+		}
+		
+		public Parallelogram (double length, double width, double height)
+		{
+			super(length, width, length, width);
+			this.height=height;
+		}
+		
+		public double area()
+		{
+			return (super.getSide2()*height);
+		}
+		
+		public void setHeight(double newHeight)
+		{
+			height=newHeight;
+		}
+		
+		public double getHeight()
+		{
+			return height;
+		}
+	}
+
+
