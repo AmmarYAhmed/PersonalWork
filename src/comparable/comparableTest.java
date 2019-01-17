a45a8aff04162ca344c824770f852fc93bd532fb
Ammar Ahmed <21ahmeda@elmbrookstudents.org>
2019-01-17 14:11:58 -0600

    test

---
+++ src/comparable/comparableTest.java
@@ -0,0 +1,20 @@
+package comparable;
+import java.util.Random;
+public class comparableTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*Worker[] workers = new Worker[100];
+		Random generator = new Random();
+		for (int j=0; j<100; j++)
+		{
+			workers[j] = new Worker(generator.nextInt(100),generator.nextDouble()*150);
+			Worker smallest = workers[0];
+			Worker largest = workers[0];
+			for (int j)
+			
+		}*/
+		Random
+	}
+
+}
